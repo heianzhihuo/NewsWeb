5ed217fc46cf9742d6f3e665e6e97cb68a3c329a
heianzhihuo <m18245027840@163.com>
2016-10-31 19:01:06 +0800

    分支1

---
+++ src/activity/SohuNews.java
@@ -0,0 +1,79 @@
+package activity;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import model.News;
+import sun.security.pkcs11.P11TlsKeyMaterialGenerator;
+
+public class SohuNews {
+
+	List<News> ShNews;
+	
+	
+	static protected void getURL(String u){
+		URL url;
+		InputStream is;
+		InputStreamReader isr;
+		BufferedReader r;
+		String str;
+		OutputStream out = null;
+		
+		Pattern p1= Pattern.compile("十二大关键词解读十八届六中全会公报");
+		Matcher m1;
+		
+		try{
+			System.out.println("Reading URL: "+u);
+			url = new URL(u);
+			is = url.openStream();
+			isr = new InputStreamReader(is);
+			r = new BufferedReader(isr);
+			out = new FileOutputStream("test.txt");
+			//out.write(r.re);
+			do {
+				str = r.readLine();
+				
+				if(str!=null){
+					byte [] temp = str.getBytes();
+					out.write(temp);
+				}
+					
+					//System.out.println(str);
+			}while(str!=null);
+		}catch(MalformedURLException e){
+			System.out.println("Must enter a valid URL");
+		}catch(IOException e){
+			System.out.println("Can't connect");
+		}finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		String input = s.nextLine();
+		System.setProperty("http.proxyHost","socks.myhost.com");
+		System.setProperty("http.proxyPort","1080");
+		getURL("http://news.sohu.com/");
+		System.out.println("Success");
+		
+	}
+
+}
--- src/model/News.java
+++ src/model/News.java
@@ -1,9 +1,64 @@
 package model;
 
 import java.sql.Date;
+import java.sql.Time;
 
 public class News {
 	private String Title;
 	private Date PublishDate;
-
+	private Time time;
+	private String Link;
+	private String Source;
+	private String Picture;
+	private int Weight;
+	private String Type;
+	
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	public Date getPublishDate() {
+		return PublishDate;
+	}
+	public void setPublishDate(Date publishDate) {
+		PublishDate = publishDate;
+	}
+	public Time getTime() {
+		return time;
+	}
+	public void setTime(Time time) {
+		this.time = time;
+	}
+	public String getLink() {
+		return Link;
+	}
+	public void setLink(String link) {
+		Link = link;
+	}
+	public String getSource() {
+		return Source;
+	}
+	public void setSource(String source) {
+		Source = source;
+	}
+	public String getPicture() {
+		return Picture;
+	}
+	public void setPicture(String picture) {
+		Picture = picture;
+	}
+	public int getWeight() {
+		return Weight;
+	}
+	public void setWeight(int weight) {
+		Weight = weight;
+	}
+	public String getType() {
+		return Type;
+	}
+	public void setType(String type) {
+		Type = type;
+	}
 }
